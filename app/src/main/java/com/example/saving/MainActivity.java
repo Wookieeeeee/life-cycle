12ba73f1bbf783a323875f3c47a298fb96c0a8ba
bobr <Felixxx0044@gmail.com>
2023-12-13 21:22:35 +0300

    SharedPreferences

--- app/src/main/java/com/example/saving/MainActivity.java
+++ app/src/main/java/com/example/saving/MainActivity.java
@@ -3,76 +3,27 @@ package com.example.saving;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE);
 
-        if (savedInstanceState != null) {
-            textView.setText(savedInstanceState.getString("textToBundle"));
-        }
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //editor.putString("newHelloText", "helloSharedPreferences");
+        editor.clear();
+        editor.apply();
 
-        Log.d("LifeCycle method: ", "onCreate!");
-        textView.append("onCreate!" + "\n");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.d("LifeCycle method: ", "onStart!");
-        textView.append("onStart!" + "\n");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.d("LifeCycle method: ", "onResume!");
-        textView.append("onResume!" + "\n");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.d("LifeCycle method: ", "onPause!");
-        textView.append("onPause!" + "\n");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.d("LifeCycle method: ", "onStop!");
-        textView.append("onStop!" + "\n");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Log.d("LifeCycle method: ", "onDestroy!");
-        textView.append("onDestroy!" + "\n");
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        Log.d("onSaveInstanceState: ", "onSaveInstanceState!");
-
-        outState.putString("textToBundle", textView.getText().toString());
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(sharedPreferences.getString("newHelloText", "DefaultText"));
     }
 }
